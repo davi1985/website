dde3485e3f37b187bd16e83ab8dcac7797494d3d
Davi Silva <davisilvaphoto@gmail.com>
2023-04-17 17:19:58 -0300

    fix text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,6 @@ import { FooterContainer } from "./styles";
 
 export const Footer = () => {
   return (
-    <FooterContainer>
-      Davi Silva 2023 - Todos os direitos reservados
-    </FooterContainer>
+    <FooterContainer>Davi Silva 2023 - All rights reserved</FooterContainer>
   );
 };
